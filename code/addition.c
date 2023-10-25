2cdc663938daa4618163f891e102cf864c9fec5f
coderooni <rafiqobaid@gmail.com>
2023-10-25 09:47:39 +0200

    Added comments to the function

--- code/addition.c
+++ code/addition.c
@@ -1,18 +1,25 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int add(int a, int b) {
-    return a + b;
+// Oh, I forgot to add comments to my file
+int add(int a, int d) {
+    return a + d;
 }
 
+// Subtract
+int sub(int a, int b) {
+    return a - b;
+}
 
+// You also forgot to add comments to this part, ya dummy
 int main(int argc, char *argv[]) {
     char *p;
     
     printf("%d\n", argc);
 
-    if (argc == 3)
+    if (argc == 3) {
         printf("%d\n", add(strtol(argv[1], &p, 10), strtol(argv[2], &p, 10)));
-
+        printf("%d\n", add(strtol(argv[1], &p, 10), strtol(argv[2], &p, 10)));
+    }
     return 0;
 }
